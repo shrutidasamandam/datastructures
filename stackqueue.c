160c86d6bb97f76e399a6963ac938cdcb7b8671b
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2022-02-20 14:52:35 +0530

    Update stackqueue.c

--- stackqueue.c
+++ stackqueue.c
@@ -3,19 +3,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void push();
-void pop();
-void display();
-void enqueue();
-void dequeue();
-void display2();
+struct qnode {
+    struct qnode *next;
+    int qdata;
+};
+struct qnode *qhead;
 
-//stack
-struct node{
-    int data; 
+void enqueue()
+{
+    struct qnode *temp = (struct qnode*)malloc(sizeof(struct qnode));
+    struct qnode *ptr;
+    if(temp == NULL)
+    {
+        printf("\nInsufficient memory");
+    }
+    printf("\nEnter data for node");
+    scanf("%d",&temp->qdata);
+    if(qhead==NULL)
+    {
+        qhead = temp;
+    } else {
+        ptr = qhead;
+        while(ptr->next!=NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = temp;
+    }
+}
+
+void dequeue()
+{
+    struct qnode*temp;
+    if(qhead==NULL)
+    {
+        printf("\nEmpty list");
+    } else {
+        temp = qhead;
+        qhead = temp->next;
+        free(temp);
+    }
+}
+
+
+void display2()
+{
+    struct qnode *temp;
+    temp = qhead;
+    if(qhead==NULL)
+    {
+        printf("\nEmpty list");
+    } else {
+        while(temp!=NULL)
+        {
+            printf("\n%d",temp->qdata);
+            temp = temp->next;
+        }
+    }
+}
+
+struct node {
     struct node *next;
+    int data;
 };
 struct node *head;
+
+void push()
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    if(temp==NULL)
+    {
+        printf("\nInsufficient memory");
+    }
+    printf("\nEnter data ");
+    scanf("%d",&temp->data);
+    temp->next = NULL;
+    if(head==NULL)
+    {
+        head = temp;
+    } else {
+        temp -> next = head;
+        head = temp;
+    }
+}
+
+void pop()
+{
+    struct node *temp;
+    if(head==NULL)
+    {
+        printf("\nEmpty list");
+    } else {
+        temp = head;
+        head = temp->next;
+        free(temp);
+    }
+}
+
+
+void display()
+{
+    struct node *temp;
+    temp = head;
+    if(head==NULL)
+    {
+        printf("\nEmpty list");
+    } else {
+        while(temp!=NULL)
+        {
+            printf("\n%d",temp->data);
+            temp = temp->next;
+        }
+    }
+}
 
 int main()
 {
@@ -48,123 +148,4 @@ int main()
         
         }
     return 0;
-}
-
-void push ()
-{
-    int data;
-    struct node *new_node = (struct node*)malloc(sizeof(struct node));
-    if(new_node==NULL)
-    {
-        printf("\nstack full");
-    } else {
-        printf("\nenter value to be inserted: ");
-        scanf("%d",&data);
-        if(head==NULL)
-        {
-            new_node->data=data;
-            new_node->next=NULL;
-            head=new_node;
-        } else {
-            new_node->data=data;
-            new_node->next=head;
-            head=new_node;
-        }
-    }
-}
-
-void pop()
-{
-    int item;
-    struct node *new_node;
-    if(head==NULL)
-    {
-        printf("\nempty stack");
-    } else {
-        item=head->data;
-        new_node=head;
-        head=head->next;
-        free(new_node);
-    }
-}
-
-void display()
-{
-    int i;
-    struct node* new_node;
-    new_node=head;
-    if(new_node==NULL)
-    {
-        printf("\nempty stack");
-    } else {
-        while(new_node!=NULL)
-        {
-            printf("\n%d",new_node->data);
-            new_node=new_node->next;
-        }
-    }
-
-}
-//queue
-struct qnode{
-    int qdata;
-    struct qnode *qnext;
-};
-struct qnode *qfront;
-struct qnode *qrear;
-
-void enqueue()
-{
-    struct qnode* ptr;
-    int item;
-    
-    ptr=(struct qnode *)malloc(sizeof(struct qnode));
-    if(ptr==NULL)
-    {
-        printf("\nqueue overflow");
-    } else {
-        printf("\nenter value");
-        scanf("%d",&item);
-        ptr->qdata=item;
-        if(qfront==NULL)
-        {
-            qfront=ptr;
-            qrear=ptr;
-            qfront->qnext=NULL;
-            qrear->qnext=NULL;
-        } else {
-            qrear->qnext=ptr;
-            qrear=ptr;
-            qrear->qnext=NULL;
-        }
-    }
-}
-
-void dequeue()
-{
-    struct qnode *ptr;
-    if(qfront==NULL)
-    {
-        printf("\nqueue empty");
-    } else {
-        ptr=qfront;
-        qfront=qfront->qnext;
-        free(ptr);
-    }
-}
-
-void display2()
-{
-    struct qnode *ptr;
-    ptr=qfront;
-    if(qfront==NULL)
-    {
-        printf("\nqueue empty");
-    } else {
-        while(ptr!=NULL)
-        {
-            printf("\n%d",ptr->qdata);
-            ptr=ptr->qnext;
-        }
-    }
 }
